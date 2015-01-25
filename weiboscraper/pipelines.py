b6c68aa527d88dce178cc6d59247668acc4211a1
Qingchao Kong <kqingchao@gmail.com>
2015-01-25 14:13:37 +0800

    write item to mongodb

--- weiboscraper/pipelines.py
+++ weiboscraper/pipelines.py
@@ -5,7 +5,42 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from scrapy import log
+from scrapy.exceptions import DropItem
 
 class WeiboscraperPipeline(object):
     def process_item(self, item, spider):
         return item
+        
+class UserInfoPipeline(object):
+    """ 用于处理用户信息的pipeline
+    
+    NOTE：spider在指定pipeline时需要注意顺序
+    """
+    def __init__(self):
+        self.name = 'user-info' # 根据此选择可处理的spider
+        
+    def process_item(self, item, spider):
+        """ 将item存储与数据库中
+        """
+        # 如果该pipelien不在spider的指定的pipeline中，则直接忽略
+        if self.name not in getattr(spider, 'pipelines', []):
+            return item
+        
+        #import ipdb; ipdb.set_trace()
+        # 检查数据库中是否有重复
+        cur = spider.collection.find({'uid':item['uid']})
+        if cur.count() > 0:
+            raise DropItem('User already in collection.')
+            
+        # 保存到数据库中
+        # TODO: item_id = spider.collection.insert(item) # 执行该句出现错误
+        item_id = spider.collection.insert(dict(item))
+        
+        return item
+        
+    def open_spider(self, spider):
+        pass
+        
+    def close_spider(self, spider):
+        pass
